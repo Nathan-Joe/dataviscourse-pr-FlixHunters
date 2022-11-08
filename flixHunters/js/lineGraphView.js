95a0b5f28c1ae0ba76b1a58f1035d67dd20e56e9
Albert Frei <albert.h.frei@gmail.com>
2022-11-07 22:50:11 -0700

    start of the line-graph

--- flixHunters/js/lineGraphView.js
+++ flixHunters/js/lineGraphView.js
@@ -1,8 +1,8 @@
 class LineGraphView {
 
   globalApplicationState;
-  width = 1000;
-  height = 350;
+  width = 700;
+  height = 500;
   
   constructor(globalApplicationState) {
     this.globalApplicationState = globalApplicationState;
@@ -11,20 +11,41 @@ class LineGraphView {
 
   setup(){
     let lineGraphSVG = d3.select("#lineGraphSVG");
+    this.yAxisPadding = 80;
+    this.xAxisPadding = 50;
   
     lineGraphSVG
       .style('width',this.width)
       .style('height',this.height)
 
-    this.adjustGraph(this.globalApplicationState.allMovieData);
     
+    let dataByYearAdded = d3.group(this.globalApplicationState.allMovieData, d => d.year_added)
 
+    this.adjustGraph(dataByYearAdded);
+    let years = []
+    for(let key in [...dataByYearAdded.keys()]) {
+      years.push(parseInt([...dataByYearAdded.keys()][key]))
+    }
+    
+    
+    this.xScale = d3.scaleTime()
+      .domain([d3.min(years), d3.max(years)])
+      .range([0, this.width - this.yAxisPadding])
+
+      console.log(this.xScale('2022'))
+
+    lineGraphSVG.select('#x-axis')
+      .append('g')
+      .attr('transform', `translate(${this.yAxisPadding}, ${this.height - this.xAxisPadding})`)
+      .call(d3.axisBottom(this.xScale)
+        .tickFormat(d3.timeFormat('%Y'))
+      );
     
   }
 
   adjustGraph(data){
-
-    console.log(data)
+    
+    
 
   }
 
