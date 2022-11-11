5df327520bede96c3aceed3225d9fece1ec46262
Namlot <52209145+Namlot@users.noreply.github.com>
2022-11-11 09:42:56 -0700

    code cleanup

--- flixHunters/js/donutView.js
+++ flixHunters/js/donutView.js
@@ -3,9 +3,12 @@ class DonutView {
   globalApplicationState;
   width = 500;
   height = 350;
+  margin = 45;
+  radius = 0;
   
   constructor(globalApplicationState) {
     this.globalApplicationState = globalApplicationState;
+    this.radius = Math.min(this.width,this.height)/2 - this.margin;
     this.setup();
   }
 
@@ -15,6 +18,8 @@ class DonutView {
     donutSVG
       .style('width',this.width)
       .style('height',this.height)
+      .append('g')
+      .attr('transform','translate(' + this.width/2 + ',' + this.height/2 + ")");
 
       this.adjustGraph(this.globalApplicationState.allMovieData);
     
@@ -22,6 +27,13 @@ class DonutView {
 
   adjustGraph(data){
 
+     //transform data
+     let groupedData = d3.group(data, d => d.rating );
+     
+
+
+      let pie = d3.pie()
+        .value(d => d.count);
  
 
   }
--- flixHunters/js/script.js
+++ flixHunters/js/script.js
@@ -5,6 +5,46 @@ async function loadData () {
   //Filter to only have movies for now 
   movieData = movieData.filter(m => m.type == 'Movie');
 
+  //Clean up ratings
+  for (const movie of movieData) {
+    switch(movie.rating){
+
+      case "PG-13":
+      case "TV-14":
+        movie.rating = "PG-13";
+        break;
+      
+      case "TV-PG":      
+      case "PG":
+      case "TV-Y7":
+        movie.rating = "PG";
+        break;
+
+      case "TV-G":
+      case "G":
+      case "TV-Y":
+        movie.rating = "G";
+        break;
+
+      case "NC-17":
+        movie.rating = "X"
+        break;
+
+      case "R":
+      case "TV-MA":
+        movie.rating = "R";
+        break;
+
+      case "NR":
+      case "":
+      case "UR":
+      default:
+        movie.rating = "NR";
+        break;
+
+    }
+  }
+
   return movieData;
 }
 
@@ -13,13 +53,19 @@ const globalApplicationState = {
   allMovieData : null,
   donutView  : null,
   barGraphView  : null,
-  lineGraphView  : null
+  lineGraphView  : null,
+  colorScale: null
 };
 
 
 loadData().then((loadedData) => {
   // Store the loaded data into the globalApplicationState
   globalApplicationState.allMovieData = loadedData;
+
+  let categories = new Set(loadedData.map(x => x.rating));
+  globalApplicationState.colorScale = d3.scaleOrdinal(d3.schemeDark2)
+    .domain(categories);
+
 
 
   // Creates the view objects with the global state passed in 
